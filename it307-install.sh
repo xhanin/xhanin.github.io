c798094934fe81b755d0743d1f9249cd3a3f3aba
xhanin <xhanin@users.noreply.github.com>
2014-10-30 11:01:55 +0100

    Update it307-install.sh

--- it307-install.sh
+++ it307-install.sh
@@ -3,10 +3,12 @@
 mkdir /tmp/it307
 cd /tmp/it307
 
-wget http://eclipse.ialto.com/eclipse/downloads/drops4/R-4.3.1-201309111000/eclipse-SDK-4.3.1-linux-gtk.tar.gz
-wget  --no-cookies --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com" http://download.oracle.com/otn-pub/java/jdk/7u45-b18/jdk-7u45-linux-i586.tar.gz
+wget http://eclipse.ialto.com/technology/epp/downloads/release/luna/SR1/eclipse-java-luna-SR1-linux-gtk.tar.gz
 
-tar xzvf eclipse-SDK-4.3.1-linux-gtk.tar.gz
-tar xzvf jdk-7u45-linux-i586.tar.gz
+tar xzvf eclipse-java-luna-SR1-linux-gtk.tar.gz
+echo "download jdk7u71 from http://www.oracle.com/technetwork/java/javase/downloads/jdk7-downloads-1880260.html"
+echo "and place downloaded tgz in /tmp/it307"
+echo "and then go into /tmp/it307 and unpack it"
+echo "tar xzvf jdk-7u71-linux-i586.tar.gz"
 
-echo "eclipse/eclipse -vm /tmp/it307/jdk1.7.0_45/jre/bin/java" > eclipse.sh
+echo "eclipse/eclipse -vm /tmp/it307/jdk1.7.0_71/jre/bin/java" > eclipse.sh
